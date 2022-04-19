8d3c9bef1fecbd8762a28967d330377213a85385
Florian <florian.vandaalen@maastro.nl>
2022-04-19 16:38:02 +0200

    added start of the java wrapper

---
+++ java/verticox/src/main/java/com/florian/verticox/webservice/VerticoxCentralServer.java
@@ -0,0 +1,6 @@
+package com.florian.verticox.webservice;
+
+import com.florian.nscalarproduct.webservice.CentralServer;
+
+public class VerticoxCentralServer extends CentralServer {
+}
---
+++ java/verticox/src/main/java/com/florian/verticox/webservice/VerticoxServer.java
@@ -0,0 +1,7 @@
+package com.florian.verticox.webservice;
+
+import com.florian.nscalarproduct.webservice.Server;
+
+public class VerticoxServer extends Server {
+    
+}
