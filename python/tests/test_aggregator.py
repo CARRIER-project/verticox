40a231d118bb37b7824ef4f887ffc853a3ae7665
Djura Smits <djura.smits@gmail.com>
2022-08-09 16:23:22 +0200

    Added docstring to test

--- python/tests/test_aggregator.py
+++ python/tests/test_aggregator.py
@@ -25,11 +25,15 @@ def test_compute_deaths_per_t_with_right_censored():
 
 
 def test_compute_deaths_per_t_with_right_censored_returns_0_deaths():
+    """
+    Right censored samples shouldn't count as a death. Therefore, if all
+    samples with the same event time are right censored, there should be 0 deaths
+    at that timestep.
+    """
     event_times = np.array([1, 1, 2])
     event_happened = np.array([True, True, False])
 
     result = Aggregator._compute_deaths_per_t(event_times, event_happened)
     result = dict(result)
-
 
     TestCase().assertDictEqual(result, {1: 2, 2: 0})
