0836adf7d3fbabfdd155b711f6cb9db3b76a8b6d
Florian <florian.vandaalen@maastro.nl>
2022-09-30 14:14:54 +0200

    updated git repo

--- Vantage6Wrapper/pushF.sh
+++
@@ -1,9 +0,0 @@
-IMAGE=harbor.carrier-mu.src.surf-hosted.nl/carrier/verticox_predictors
-
-docker build --build-arg SSH_PRIVATE_KEY="$(cat C:\\Florian\\GIT\\KEY\\id_rsa)" \
-  --build-arg SSH-PUBLIC-KEY="$(cat C:\\Florian\\GIT\\KEY\\id_rsa.pub)" \
-  -t verticox_predictor  .
-
-docker tag verticox_predictor $IMAGE
-
-docker push $IMAGE
