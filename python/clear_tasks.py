e926bfd6ee032dcc0253520d4b123a62750fd32f
Djura Smits <djura.smits@gmail.com>
2022-10-13 11:57:03 +0200

    Created script to clear old tasks

---
+++ python/clear_tasks.py
@@ -0,0 +1,26 @@
+import clize
+from vantage6.client import Client
+
+
+def run_verticox_v6(host, port, user, password, private_key):
+    client = Client(host, port)
+    client.authenticate(user, password)
+    client.setup_encryption(private_key)
+
+    page = 1
+
+    while True:
+        tasks = client.task.list()
+
+        if len(tasks['data']) == 0:
+            break
+
+        print(f'Deleting {len(tasks["data"])} tasks')
+
+        for t in tasks['data']:
+            client.task.delete(t['id'])
+
+
+
+if __name__ == '__main__':
+    clize.run(run_verticox_v6)
