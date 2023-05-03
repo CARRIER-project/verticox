c7519bb7f6197374d909d7760ec6dbe137e62659
Djura Smits <djura.smits@gmail.com>
2023-05-03 13:09:23 +0200

    Explain that group_samples_at_risk does not differentiate between right-censored or not

--- python/verticox/common.py
+++ python/verticox/common.py
@@ -13,7 +13,8 @@ def group_samples_at_risk(event_times: ArrayLike) -> types.DictType(types.float6
     """
     Groups the indices of samples on whether they are at risk at a certain time.
 
-    A sample is at risk at a certain time when its event time is greater or equal that time.
+    A sample is at risk at a certain time when its event- or censor time is greater or equal that
+    time.
 
     Ri is the set of indices of samples with death or censor times occurring
     after ti.
