cc62d6c0b05f971b85458409392ca6cab1586b66
Djura Smits <djura.smits@gmail.com>
2024-02-21 10:47:20 +0100

    Created function that will select columns in a dataset in a prioritized fashion

--- python/tests/test_datasets.py
+++ python/tests/test_datasets.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
-from verticox.datasets import load_aids_data_with_dummies, get_test_dataset
+from verticox.datasets import load_aids_data_with_dummies, get_test_dataset, \
+    get_prioritized_features
 
 
 def test_load_aids_data_converts_dummies():
@@ -14,3 +15,14 @@ def test_get_test_dataset_get_all_features_dummies():
     features, outcome, column_names = get_test_dataset(10, feature_limit=10, dataset="aids")
 
     assert len(column_names) == 10
+
+
+def test_dummy_features_are_mixed():
+    # Dataframe with 2 categorical columns
+    df = pd.DataFrame({"animal": ["dog", "cat"], "sex": ["female", "male"]})
+
+    prioritized = get_prioritized_features(df)
+
+    target_column_order = ["cat", "female", "dog", "male"]
+
+    assert list(prioritized.columns) == target_column_order
--- python/verticox/datasets.py
+++ python/verticox/datasets.py
@@ -2,10 +2,53 @@ from typing import Tuple, List
 
 import numpy as np
 import pandas as pd
-from numpy._typing import ArrayLike
 from sksurv.datasets import load_aids, get_x_y, load_whas500
 
-from verticox.common import SEER, NotEnoughFeaturesException, _uncensored
+from verticox.common import SEER, _uncensored
+
+
+def get_dummies(categorical_features: pd.DataFrame):
+    for c in categorical_features.columns:
+        categorical_features[c] = categorical_features[c].astype("category")
+
+    all_dummies = []
+    columns = categorical_features.columns
+
+    for c in columns:
+        dummies = pd.get_dummies(categorical_features[c])
+        all_dummies.append(dummies)
+
+    idx = 0
+    num_dummies = len(all_dummies)
+
+    while not all_empty_dataframes(all_dummies):
+        current_dummies = all_dummies[idx]
+        if len(current_dummies) == 0:
+            idx = (idx + 1) % num_dummies
+            continue
+
+        column_name = current_dummies.columns[0]
+        column = current_dummies[column_name]
+        current_dummies.drop(columns=[column_name], inplace=True)
+        idx = (idx + 1) % num_dummies
+        yield column
+
+
+def get_prioritized_features(df: pd.DataFrame):
+    numerical_features = df.select_dtypes(include="number")
+    other_features = df.drop(columns=numerical_features.columns)
+
+    sorted_dummies = list(get_dummies(categorical_features=other_features))
+
+    return pd.concat([numerical_features] + sorted_dummies, axis=1)
+
+
+def all_empty_dataframes(df):
+    for df in df:
+        if len(df.columns) > 0:
+            return False
+
+    return True
 
 
 def load_aids_data_with_dummies(endpoint: str = "aids") -> (pd.DataFrame, np.array):
@@ -38,7 +81,6 @@ def load_seer() -> (pd.DataFrame, np.array):
     Returns:
 
     """
-
     df = pd.read_csv(
         "https://zenodo.org/records/5120960/files/SEER%20Breast%20Cancer%20Dataset%20.csv?download=1")
     # Remove empty column
@@ -129,3 +171,26 @@ def get_test_dataset(
         features = features[columns]
 
     return features.values.astype(float), events, list(features.columns)
+
+
+def unpack_events(events):
+    """
+    Unpacks outcome arrays from sksurv into two separate arrays with censor and event time
+    :param events:
+    :return: (times array, status array)
+    """
+    times = []
+    right_censored = []
+
+    for event in events:
+        times.append(event[1])
+        right_censored.append(event[0])
+
+    right_censored = np.array(right_censored)
+    if right_censored.dtype != bool:
+        raise Exception('Status is not boolean.')
+    return np.array(times), right_censored
+
+
+class NotEnoughFeaturesException(Exception):
+    pass
