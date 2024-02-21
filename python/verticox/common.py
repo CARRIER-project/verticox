03e5b80149cb1071f665d43297279510b05ff39d
Djura Smits <djura.smits@gmail.com>
2024-02-21 16:29:57 +0100

    Removed some annoying prints

--- python/verticox/common.py
+++ python/verticox/common.py
@@ -56,9 +56,6 @@ def group_samples_on_event_time(
 
     """
     Dt = {}
-    print("Grouping samples on event time")
-    print(f"event times{event_times}")
-    print(f"event happened: {event_happened}")
 
     for idx, events in enumerate(zip(event_times, event_happened)):
         t, i = events
